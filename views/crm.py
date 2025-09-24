50b3d72eb545dba680ded9db83db263063d81c43
pabloedu01 <pablo@pabloedu.com>
2025-09-24 15:48:23 +0000

    Adicionado filtro por tipo

--- views/crm.py
+++ views/crm.py
@@ -29,8 +29,8 @@ def eventos_showroom():
         
         token = session.get('token')
 
-        url = f"https://backend.caiuas.com.br/api/crm/eventos_showroom?{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}"
-        context['current_page'] = f'https://app.caiuas.com.br/crm/eventos_showroom?{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}'
+        url = f"https://backend.caiuas.com.br/api/crm/eventos_showroom?&tipo_evento=785,787{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}"
+        context['current_page'] = f'https://app.caiuas.com.br/crm/eventos_showroom?&tipo_evento=785,787{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}'
         payload = {}
         headers = {
             "Authorization": f"Bearer {token}"
