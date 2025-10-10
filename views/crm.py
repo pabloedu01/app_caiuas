f193b7dcb599503545c10c2ed93447a2edbfefaa
pabloedu01 <pablo@pabloedu.com>
2025-10-10 18:29:45 +0000

    Adicionado rota para pesquisa de satisfacao

--- views/crm.py
+++ views/crm.py
@@ -29,8 +29,8 @@ def eventos_showroom():
         
         token = session.get('token')
 
-        url = f"https://backend.caiuas.com.br/api/crm/eventos_showroom?&tipo_evento=785,787,793,795,797,799{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}"
-        context['current_page'] = f'https://app.caiuas.com.br/crm/eventos_showroom?&tipo_evento=785,787,793,795,797,799{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}'
+        url = f"https://backend.caiuas.com.br/api/crm/eventos?&tipo_evento=785,787,793,795,797,799{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}"
+        context['current_page'] = f'https://app.caiuas.com.br/crm/eventos?&tipo_evento=785,787,793,795,797,799{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}'
         payload = {}
         headers = {
             "Authorization": f"Bearer {token}"
@@ -77,3 +77,35 @@ def show_eventos_showroom(evento_id):
     except Exception as e:
         return render_template('500.html', error=str(e))
     
+@crm_bp.route('/crm/pesquisa_satisfacao', methods=['GET'])
+@token_required
+def list_pesquisa_satisfacao():
+    try:
+        status = request.args.get('status', None)
+        initial_date = request.args.get('initial_date', None)
+        final_date = request.args.get('final_date', None)
+        current_page = request.args.get('current_page', None)
+        search = request.args.get('search', None)
+        limit = request.args.get('limit', None)
+
+        context = {}
+        context['token_data'] = request.token_data
+        context['title'] = "Pesquisa de Satisfação"
+        
+        token = session.get('token')
+
+        url = f"https://backend.caiuas.com.br/api/crm/eventos?&tipo_evento=38,180,30{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}"
+        context['current_page'] = f'https://app.caiuas.com.br/crm/eventos?&tipo_evento=38,180,30{f'&search={search}' if search else ''}{f'&status={status}' if status else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}{f'&initial_date={initial_date}' if initial_date else ''}{f'&final_date={final_date}' if final_date else ''}{f'&search={search}' if search else ''}'
+        payload = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        
+        data = response.json()
+        context['token'] = token
+        context['status_response'] = response.status_code
+        context['data'] = data
+        return render_template('crm/pesquisa_satisfacao.html', context=context)
+    except Exception as e:
+        return render_template('500.html', error=str(e))
