247925c42b9568ad48fc005866a60314fae7718a
pabloedu01 <pablo@pabloedu.com>
2025-10-24 19:16:11 +0000

    Adicionado visualização de veículos

---
+++ views/veiculos.py
@@ -0,0 +1,75 @@
+from flask import Blueprint, jsonify, request, render_template, session
+from datetime import datetime, timedelta
+from database import postgres_chatwoot, postgres_site
+import json
+import os
+import requests
+from dotenv import load_dotenv
+import tempfile
+import urllib.request
+from auth import token_required
+
+load_dotenv()
+veiculos_bp = Blueprint('veiculos', __name__)
+
+@veiculos_bp.route('/veiculos/produtos', methods=['GET'])
+@token_required
+def list_veiculos_produtos():
+    try:
+        search = request.args.get('search', None)
+        current_page = request.args.get('current_page', None)
+        limit = request.args.get('limit', None)
+        
+        context = {}
+        context['token_data'] = request.token_data
+        context['title'] = "Cadastro de Veículos"
+        token = session.get('token')
+        
+        url = f"https://backend.caiuas.com.br/api/veiculos/produtos?{f'&search={search}' if search else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}"
+        context['current_page'] = f'https://backend.caiuas.com.br/api/veiculos/produtos?{f'&search={search}' if search else ''}{f'&limit={limit}' if limit else ''}{f'&current_page={current_page}' if current_page else ''}'
+        payload = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        
+        data = response.json()
+        
+        context['token'] = token
+        context['status_response'] = response.status_code
+        context['data'] = data
+        # return context
+        return render_template('veiculos/produtos.html', context=context)
+    except Exception as e:
+        return render_template('500.html', error=str(e))
+
+@veiculos_bp.route('/veiculos/produtos/<int:cod_modelo>', methods=['GET'])
+@token_required
+def show_veiculos_produtos(cod_modelo):
+    try:
+        search = request.args.get('search', None)
+        current_page = request.args.get('current_page', None)
+        limit = request.args.get('limit', None)
+        
+        context = {}
+        context['token_data'] = request.token_data
+        context['title'] = "Atualização de veículo"
+        token = session.get('token')
+        
+        
+        url = f"https://backend.caiuas.com.br/api/veiculos/produtos/{cod_modelo}"
+        payload = {}
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        
+        data = response.json()
+        
+        context['token'] = token
+        context['status_response'] = response.status_code
+        context['data'] = data
+        # return context
+        return render_template('veiculos/show_produtos.html', context=context)
+    except Exception as e:
+        return render_template('500.html', error=str(e))
